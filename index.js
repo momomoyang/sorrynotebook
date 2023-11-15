a19f763988d4aea8763cb5ca953e46e2536b6caf
momomoyang <jiyu804@naver.com>
2023-11-15 14:34:21 +0900

    feat : 내건안되고 복붙은 잘됨 슬퍼

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 window.initmap = function () {
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 40.6970193, lng: -74.3093291 },
+        center: { lat: 37.5400456, lng: 126.9921017 },
         zoom: 10,
     });
 
     const Land mark = [
-        { label: "B", name: "타임스퀘어", lat: 40.7579747, lng: -73.9855426 }
-        { label: "C", name: "자연사박물관", lat: 40.7813241, lng: -73.9739882 }
-        { label: "D", name: "엠파이어 스테이트 빌딩", lat:40.7499416, lng: -73.9889839 }
+    { label: "C", name: "코엑스몰", lat: 37.5115557, lng: 127.0595261 },
+    { label: "G", name: "고투몰", lat: 37.5062379, lng: 127.0050378 },
+    { label: "D", name: "동대문시장", lat: 37.566596, lng: 127.007702 },
+    { label: "I", name: "IFC몰", lat: 37.5251644, lng: 126.9255491 },
+    { label: "L", name: "롯데월드타워몰", lat: 37.5125585, lng: 127.1025353 },
+    { label: "M", name: "명동지하상가", lat: 37.563692, lng: 126.9822107 },
+    { label: "T", name: "타임스퀘어", lat: 37.5173108, lng: 126.9033793 }
     ];
 
     const bounds = new google. maps LatLngBounds();
-    const infowindow = new google.maps.Infowindow();
+    const infowindow = new google.maps.InfoWindow();
 
-    Land mark.forEach(({ label, name, lat, lng }) => {
+    malls.forEach(({ label, name, lat, lng }) => {
         const marker = new google.maps.Marker({
-            position: { lat, lng}
+            position: { lat, lng }
             label,
             map
     });
-    bounds.extend(marker.position)
+    bounds.extend(marker.position);
 
     marker.addListner("click", () => {
         map.panTo(marker.position);
@@ -29,7 +33,7 @@ window.initmap = function () {
             map
         });
     });
-    });
+});
 
     map.fitBounds(bounds);
 };
